c84ab666ddc6c7f62db2305db77bbaece6216367
Wayne Links <wayne.links@gmail.com>
2019-11-10 21:52:07 +0200

    Refactor apiRequest middleware to accept api requests from signup action creator

--- client/src/shared/redux/actions/api.js
+++ client/src/shared/redux/actions/api.js
@@ -1,8 +1,14 @@
 import { API_REQUEST, API_SUCCESS, API_ERROR } from '../constants'
 
-export const apiRequest = ({ email, password }, api, method, url, entity) => ({
+export const apiRequest = (
+  { name, email, password },
+  api,
+  method,
+  url,
+  entity
+) => ({
   type: `${entity} ${API_REQUEST}`,
-  payload: { email, password, api, method, url, entity }
+  payload: { name, email, password, api, method, url, entity }
 })
 
 export const apiSuccess = (response, entity) => ({
