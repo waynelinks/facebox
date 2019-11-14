df3c39a41c02cdaea89fc6aeb5b774608446dcf0
Wayne Links <wayne.links@gmail.com>
2019-11-14 19:45:52 +0200

    Add displayBox over image

--- client/src/shared/components/FaceRecognizer/index.js
+++ client/src/shared/components/FaceRecognizer/index.js
@@ -3,16 +3,26 @@ import { connect } from 'react-redux'
 
 import './index.css'
 
-const FaceRecognizer = ({ image }) => {
+const FaceRecognizer = ({ image, box }) => {
   return (
     <div className='facebox'>
       <img id='image' src={image} alt='' />
+      <div
+        className='bounding-box'
+        style={{
+          top: box.topRow,
+          right: box.rightCol,
+          bottom: box.bottomRow,
+          left: box.leftCol
+        }}
+      ></div>
     </div>
   )
 }
 
 const mapStateToProps = ({ image }) => ({
-  image: image.image
+  image: image.image,
+  box: image.box
 })
 
 export default connect(mapStateToProps)(FaceRecognizer)
